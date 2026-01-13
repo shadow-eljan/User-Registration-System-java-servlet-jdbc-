c077681b4f3da064491517ff24f551bdad6cf26a
shadow-eljan <eljanrai25@gmail.com>
2026-01-13 19:14:55 +0545

    Servlet for User search form

---
+++ src/main/java/com/servlet/app/web/UserSearchFormServlet.java
@@ -0,0 +1,18 @@
+package com.servlet.app.web;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users/search")
+public class UserSearchFormServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/user-search.html");
+        rd.forward(req, resp);
+    }
+}
